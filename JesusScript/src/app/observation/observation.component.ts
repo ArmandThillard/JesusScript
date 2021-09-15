3287f2566d412ce4a7fcf6f02316079bc6e4870d
Armand <thillard.armand@gmail.com>
2021-09-15 09:34:47 +0200

    observation class + component

--- JesusScript/src/app/observation/observation.component.ts
+++ JesusScript/src/app/observation/observation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observation } from '../classes/Observation';
 
 @Component({
   selector: 'app-observation',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObservationComponent implements OnInit {
 
+  _observation: Observation
+
   constructor() { }
+
+  get code() {
+    return this._observation.code.coding[0]
+  }
+
+  get observation() {
+    return this._observation;
+  }
+
+  @Input()
+  set observation(value:Observation){
+    this._observation = value;
+  }
 
   ngOnInit(): void {
   }
