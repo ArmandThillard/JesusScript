bb39df98efbaf8fd9b5265b7fdb754cfc028e622
martinBorde <martin.borde@etud.univ-jfc.fr>
2021-09-15 09:29:57 +0200

    Fin de la présentation + afficgae du profil

--- JesusScript/src/app/infos-patient/infos-patient.component.ts
+++ JesusScript/src/app/infos-patient/infos-patient.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Input, Output} from '@angular/core';
-import {Patient} from './../classes';
+import {Patient} from './../classes/Patient';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 @Component({
   selector: 'app-infos-patient',
   templateUrl: './infos-patient.component.html',
@@ -9,14 +14,16 @@ import {Patient} from './../classes';
 export class InfosPatientComponent implements OnInit {
 
   constructor() { }
-
+  subtitleProfil : string = "Profil : ";
   _patient : Patient;
+  dateNaissanceFrench:string;
 
   @Input()
   set patient(value:Patient){
     this._patient = value;
-  }
 
+    this.dateNaissanceFrench=this._patient.birthDate;
+  }
   ngOnInit(): void {
   }
 
